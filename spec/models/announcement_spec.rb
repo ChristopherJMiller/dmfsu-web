c5561b729984a24fc1a8df9ebaf06559b95788d0
Christopher Miller <admin@mctherealm.net>
2018-01-13 18:29:00 -0500

    Added announcement model tests

--- spec/models/announcement_spec.rb
+++ spec/models/announcement_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Announcement, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:announcement) { FactoryGirl.build(:announcement) }
+
+  it 'is valid with a valid title and post' do
+    expect(announcement).to be_valid
+  end
+
+  it 'is not valid when missing a title' do
+    new_announcement = announcement
+    new_announcement.title = nil
+    expect(announcement).to_not be_valid
+  end
+
+  it 'is not valid when missing a post body' do
+    new_announcement = announcement
+    new_announcement.post = nil
+    expect(announcement).to_not be_valid
+  end
+
+  it 'is not valid when missing an author' do
+    new_announcement = announcement
+    new_announcement.admin = nil
+    expect(announcement).to_not be_valid
+  end
 end
